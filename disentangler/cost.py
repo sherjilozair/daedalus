50330b09082003f39ab73577cd04c82122242a45
Sherjil Ozair <sherjilozair@gmail.com>
2014-06-29 21:22:33 +0530

    uniformity

---
+++ disentangler/cost.py
@@ -0,0 +1,20 @@
+import numpy
+# z is shape (n, d)
+def uniformity(z):
+	n = z.shape[0]
+	d = z.shape[1]
+	t1 = (4./3.)**d
+	t2 = -2. * (1. + 2. * z - 2. * z**2).prod(axis=1).sum(axis=0) / n
+	t4 = (1 - abs(z[:, numpy.newaxis] - z)).prod(axis=2).sum()
+	t3 = (2.**d) * (t4) / (n**2)
+	return t1 + t2 + t3
+
+sigmoid = lambda x: 1. / (1. + numpy.exp(-x))
+sigmoid = lambda x: numpy.exp(x) / (1. + numpy.exp(x))
+
+z1 = numpy.random.uniform(size=(1000, 5))
+z2 = z1 + numpy.random.normal(0, 0.1, size=(1000, 5))
+
+
+print uniformity(z1)
+print uniformity(z2)
