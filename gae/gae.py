41bfc33ff84b89fd7efce08a6ac0c613e6e3d43f
Sherjil Ozair <sherjilozair@gmail.com>
2014-09-27 09:54:21 +0530

    .

--- gae/gae.py
+++ gae/gae.py
@@ -16,39 +16,12 @@ from theano.tensor.shared_randomstreams import RandomStreams
 rng_theano = RandomStreams()
 rng_numpy = numpy.random
 
-class SplSgn(UnaryScalarOp):
-    def impl(self, x):
-        #casting to output type is handled by filter
-        return numpy.sign(x)
-
-    def grad(self, (x, ), (gz, )):
-        if x.type in continuous_types:
-            return gz,
-        else:
-            return None,
-
-    def c_code(self, node, name, (x, ), (z, ), sub):
-        #casting is done by compiler
-        #TODO: use copysign
-        type = node.inputs[0].type
-        if type in float_types:
-            return "%(z)s = (%(x)s >= 0) ? (%(x)s == 0) ? 0.0 : 1.0 : -1.0;" % locals()
-        if type in int_types:
-            return "%(z)s = (%(x)s >= 0) ? (%(x)s == 0) ? 0 : 1 : -1;" % locals()
-        raise TypeError()  # complex has no sgn
-
-    def c_code_cache_version(self):
-        s = super(SplSgn, self).c_code_cache_version()
-        if s:
-            return (3,) + s
-        else:  # if parent is unversioned, we are too
-            return s
-
-splsgn = SplSgn(same_out_nocomplex, name='splsgn')
-
 sigm = T.nnet.sigmoid
 tanh = T.tanh
 ce = lambda x, y: T.nnet.binary_crossentropy(x, y)
+
+NOISE = 0.0
+DIMZ = 200
 
 class Layer():
     def __init__(self, n_in, n_out, act):
@@ -80,7 +53,7 @@ class MLP():
 
 class Multinomial():
     def __init__(self, dim):
-        self.W = theano.shared(numpy.random.normal(size=(dim, ), scale=0.00001))
+        self.W = theano.shared(numpy.random.normal(size=(dim, ), scale=0.1))
         self.params = [self.W]
 
     def get_cost(self, x):
@@ -91,31 +64,35 @@ def corrupt_SnP(x, corruption_level):
     if corruption_level == 0.0:
         return x
     a = rng_theano.binomial(size=x.shape, n=1, p=1-corruption_level)
-    #b = rng_theano.binomial(size=x.shape, n=1, p=0.5)
-    b = rng_theano.uniform(size=x.shape, low=0.0, high=1.0)
+    b = rng_theano.binomial(size=x.shape, n=1, p=0.5)
+    #b = rng_theano.uniform(size=x.shape, low=0.0, high=1.0)
     return x * a + T.eq(a, 0) * b
 
 class GAE():
-    def __init__(self, dimX, dimH):
-        self.encoder = MLP(dimX, dimH, [1200], [tanh, sigm])
-        self.decoder = MLP(dimH, dimX, [1200], [tanh, sigm])
+    def __init__(self, dimX, dimH, sizes, acts):
+        self.dimX = dimX
+        self.encoder = MLP(dimX, dimH, sizes, acts)
+        self.decoder = MLP(dimH, dimX, sizes, acts)
         self.toplayer = Multinomial(dimH)
         self.X = T.fmatrix('X')
         self.lr = T.scalar('lr')
         self.H = self.encoder(self.X)
-        self.SH = T.sgn(self.H)
-        self.RX = self.decoder(self.SH)
-        cost_recons = (ce(self.RX, self.X).sum(axis=1)/784).mean(axis=0)
-        cost_prior = (self.toplayer.get_cost(self.SH).sum(axis=1)/784).mean(axis=0)
+        self.SH = (T.ident(2.*self.H-1.)+1.)/2.
+        self.CSH = corrupt_SnP(self.SH, NOISE)
+        self.RX = self.decoder(self.CSH)
+        cost_recons = ce(self.RX, self.X).sum(axis=1).mean(axis=0)
+        cost_prior = self.toplayer.get_cost(self.SH).sum(axis=1).mean(axis=0)
 
         #cost_derivative = ((self.H - self.H**2).sum(axis=1)/784).mean(axis=0)
         cost = cost_recons + cost_prior# + cost_derivative
+        cost = cost / 784.
         params = self.encoder.params + self.decoder.params + self.toplayer.params
         grads = T.grad(cost, params)
         updates = map(lambda (param, grad): (param, param - self.lr * grad), zip(params, grads))
         self.train_fn = theano.function([self.X, self.lr], [cost_recons, cost_prior], updates=updates)
+        self.encode_fn = theano.function([self.X], self.SH)
 
-    def train(self, dataset, mbsz, epochs, lr):
+    def train(self, dataset, epochs, mbsz, lr, lr_scale):
         for e in xrange(epochs):
             rng_numpy.shuffle(dataset)
             t1 = time.clock()
@@ -126,17 +103,20 @@ class GAE():
                 #print cost
                 costs += cost
             t2 = time.clock()
-            self.dump_recons(dataset, 15, 15, e)
-            self.dump_samples(15, 15, e)
+            if self.dimX == 784:
+                self.dump_recons(dataset, 15, 15, e)
+                self.dump_samples(15, 15, e)
             print e, (t2 - t1), costs / float(len(range(0, len(dataset), mbsz))), lr
-            lr *= 0.99
+            lr *= lr_scale
 
     def dump_recons(self, D, x, y, name):
-        DD = self.decoder(self.encoder(D[:x*y])).eval()
+        h = self.encoder(D[:x*y])
+        sh = (T.ident(2.*h-1.)+1.)/2.
+        DD = self.decoder(sh).eval()
         draw_mnist(DD, 'recons/', x, y, name)
 
     def dump_samples(self, x, y, name):
-        w = sigm(model.toplayer.W).eval()
+        w = sigm(self.toplayer.W).eval()
         r = numpy.random.uniform(size=(x*y, w.shape[0]))
         h = (r < w).astype('float32')
         s = self.decoder(h).eval()
@@ -157,10 +137,17 @@ def draw_mnist(samples, output_dir, num_samples, num_chains, name):
 
 
 if __name__ == '__main__':
-    model = GAE(784, 200)
+    model1 = GAE(784, 200, [1200], [tanh, sigm])
+    model2 = GAE(200, 200, [500], [tanh, sigm])
+    model3 = GAE(200, 200, [500], [tanh, sigm])
     D = numpy.load("../mnist.npy")
-    D = (D > 0.5).astype('float32')
-    model.train(D, 100, 100, 1.)
+    model1.train(D, 200, 100, 1., .995)
+    H1 = model1.encode_fn(D).astype('float32')
+    model2.train(H1, 200, 100, 1., .995)
+    H2 = model2.encode_fn(H1).astype('float32')
+    model3.train(H2, 200, 100, 1., .995)
+    H3 = model3.encode_fn(H2).astype('float32')
+    
 
 
 
